f6e15625642b6cffd9976b56e8bc525675de11c0
rohitsoni98 <rohit.soni@credgenics.com>
2023-05-04 18:06:02 +0530

    minify changes

--- dist/cjs/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { toast } from "./core/toast";
-export * from "./headless";
-export { ToastBar } from "./components/toast-bar";
-export { ToastIcon } from "./components/toast-icon";
-export { Toaster } from "./components/toaster";
-export { toast };
-export default toast;
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
@@ -12,12 +12,6 @@ export default [
   {
     input: "src/index.ts",
     output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-        name: "cg-toast",
-      },
       {
         file: packageJson.module,
         format: "esm",
